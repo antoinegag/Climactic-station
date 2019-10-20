1f0fc1990e74462e216bb8644e3f219f5b2c7b6a
Antoine Gagnon <antoine.gagnon760@gmail.com>
2019-10-20 16:23:30 -0400

    Add endpoint to check if node

--- sara-esp.ino
+++ sara-esp.ino
@@ -16,7 +16,7 @@ BME280I2C bme;    // Default : forced mode, standby time = 1000 ms
 const char* ssid = STASSID;
 const char* password = STAPSK;
 
-const String version = "0.3.1";
+const String version = "0.3.2";
 
 ESP8266WebServer server(80);
 
@@ -137,6 +137,11 @@ void setup(void) {\
   server.on("/beep", []() {
     beep();
     server.send(200, "text/json", "{\"done\": true}");
+  });
+
+  // Used by the master server to verify if a sara-air node
+  server.on("/sara-air-node", []() {
+    server.send(200, "text/json", "{\"sara-air-node\": true}");
   });
  
   //  server.onNotFound(handleNotFound);
